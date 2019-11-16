08757118383b5f4660e47e712cf110036619086c
kanzaki923 <nk.misaka+github@gmail.com>
2019-11-17 03:38:43 +0900

    add: middleware
    ・helmetを追加
    ・mysqlを追加
    ・dotenvを追加

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv').config();
+const helmet = require('helmet'); 
 
 var indexRouter = require('./routes/index');
 
@@ -12,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
